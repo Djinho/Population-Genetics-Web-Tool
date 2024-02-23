626a057c85b54029a0e42da7f467704ee38945e5
dhwani229 <157715542+dhwani229@users.noreply.github.com>
2024-02-23 20:55:58 +0000

    added a counter to the snp analysis page

--- app/static/js/snp.js
+++ app/static/js/snp.js
@@ -36,6 +36,41 @@ function initializeTableFiltering() {
     geneNameSearch.on('keyup', debounce(filterTable, 250));
 }
 
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let maxGenes = 100;
+    const counterDisplay = document.getElementById('maxCounter');
+  
+    // Function to update the counter
+    function updateCounter() {
+      const selectedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked').length;
+      console.log('Selected Checkboxes:', selectedCheckboxes); // Debugging line
+      counterDisplay.textContent = maxGenes - selectedCheckboxes;
+    }
+  
+    // Add change event listener to all checkboxes
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    console.log('Total Checkboxes:', checkboxes.length); // Debugging line
+  
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.addEventListener('change', () => {
+        console.log('Checkbox ' + index + ' changed'); // Debugging line
+        if(document.querySelectorAll('input[type="checkbox"]:checked').length > maxGenes) {
+          alert('Maximum limit reached');
+          checkbox.checked = false;
+        }
+        updateCounter();
+      });
+    });
+  
+    // Initialize the counter for the first time
+    updateCounter();
+  });
+  
+  
+
+  
 // This function captures the selected SNP IDs and submits them with the form
 function captureAndSubmitSelectedSNPs() {
     $('form').submit(function(e) {
